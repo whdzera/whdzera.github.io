fe3560b543195fa864519cff772e8758926e1567
whdzera <whdzera@gmail.com>
2025-06-29 02:45:45 +0700

    typewrite changed

--- app/javascript/controllers/typewriter_controller.js
+++ app/javascript/controllers/typewriter_controller.js
@@ -2,35 +2,40 @@ import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
   static targets = ["text"];
-  static values = { words: Array };
+  static values = {
+    words: Array,
+    loop: { type: Boolean, default: true },
+  };
 
   connect() {
     this.index = 0;
     this.isDeleting = false;
     this.txt = "";
-    this.wordsList = this.wordsValue || ["My Blog", "whdzera"];
+    this.wordsList = this.wordsValue.length ? this.wordsValue : ["Hello World"];
     this.type();
   }
 
   type() {
-    const current = this.index % this.wordsList.length;
-    const fullTxt = this.wordsList[current];
+    const currentWord = this.wordsList[this.index % this.wordsList.length];
+    const fullTxt = currentWord;
 
     this.txt = this.isDeleting
       ? fullTxt.substring(0, this.txt.length - 1)
       : fullTxt.substring(0, this.txt.length + 1);
 
-    this.textTarget.textContent = this.txt;
+    this.textTarget.innerHTML = `${this.txt}<span class="blinking-cursor">|</span>`;
 
-    let speed = this.isDeleting ? 50 : 120;
+    let speed = this.isDeleting ? 50 : 100;
 
     if (!this.isDeleting && this.txt === fullTxt) {
-      speed = 1000;
+      speed = 2400;
       this.isDeleting = true;
     } else if (this.isDeleting && this.txt === "") {
       this.isDeleting = false;
       this.index++;
-      speed = 500;
+      speed = 1100;
+
+      if (!this.loopValue && this.index >= this.wordsList.length) return;
     }
 
     setTimeout(() => this.type(), speed);
