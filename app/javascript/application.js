b898be40d6c86a3a9aff8e523c2a10a6efd34280
whdzera <whdzera@gmail.com>
2025-09-16 22:58:02 +0700

    stimulus import meta glob

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,15 +1,18 @@
 import { Application } from "@hotwired/stimulus";
+
 window.Stimulus = Application.start();
 
-import ThemeController from "./controllers/theme_controller.js";
-Stimulus.register("theme", ThemeController);
-import NavbarController from "./controllers/navbar_burger_controller.js";
-Stimulus.register("navbar", NavbarController);
-import MessageController from "./controllers/message_controller.js";
-Stimulus.register("message", MessageController);
-import ThumbnailController from "./controllers/thumbnail_controller.js";
-Stimulus.register("thumbnail", ThumbnailController);
-import LoadingController from "./controllers/loading_controller.js";
-Stimulus.register("loading", LoadingController);
-import CopyController from "./controllers/copy_controller.js";
-Stimulus.register("copy", CopyController);
+const controllers = import.meta.glob("./controllers/*_controller.js", {
+  eager: true,
+});
+
+for (const path in controllers) {
+  const controller = controllers[path].default;
+  const name = path
+    .split("/")
+    .pop()
+    .replace("_controller.js", "")
+    .replace(/_/g, "-");
+
+  Stimulus.register(name, controller);
+}
